5e48277199e4983027ec55a13d23aaf0f7ad5d87
Maung Htike <maunghtike1999@gmail.com>
2022-04-30 18:07:24 -0700

    add workspace db and tables

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS workspace_db;
+CREATE DATABASE workspace_db;
+
+USE workspace_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id);
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
+    manager_id INT,
+);
