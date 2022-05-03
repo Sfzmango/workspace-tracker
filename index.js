13d6bd1b6ec3b2be47d40cde9a60e0d07fe0b784
Maung Htike <maunghtike1999@gmail.com>
2022-05-02 22:29:19 -0700

    add inquirer wireframe w/ loop

--- index.js
+++ index.js
@@ -16,3 +16,54 @@ const db = mysql.createConnection(
     console.log('Connected to the workspace_db database.')
 );
 
+// options
+const menuArr = ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"];
+
+function appMenu() {
+    inquirer
+        .prompt([
+            {
+                name: 'selectedOption',
+                type: 'list',
+                message: "Please select an option: ",
+                choices: menuArr,
+            }
+        ])
+        .then((res) => {
+            console.log(res);
+            if (res.selectedOption === menuArr[7]) {
+                console.log("Thank you for using this program!");
+                process.exit(1);
+            }
+            else if (res.selectedOption === menuArr[0]) {
+                console.log("option 1");
+                appMenu();
+            }
+            else if (res.selectedOption === menuArr[1]) {
+                console.log("option 2");
+                appMenu();
+            }
+            else if (res.selectedOption === menuArr[2]) {
+                console.log("option 3");
+                appMenu();
+            }
+            else if (res.selectedOption === menuArr[3]) {
+                console.log("option 4");
+                appMenu();
+            }
+            else if (res.selectedOption === menuArr[4]) {
+                console.log("option 5");
+                appMenu();
+            }
+            else if (res.selectedOption === menuArr[5]) {
+                console.log("option 6");
+                appMenu();
+            }
+            else if (res.selectedOption === menuArr[6]) {
+                console.log("option 7");
+                appMenu();
+            }
+        })
+}
+
+appMenu();
