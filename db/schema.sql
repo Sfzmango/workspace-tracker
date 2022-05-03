170d8717d95daeeaf2b6f780c693ff1b0fd92d31
Maung Htike <maunghtike1999@gmail.com>
2022-05-02 20:50:35 -0700

    fix mysql sourcing seeds issues

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,28 @@ CREATE DATABASE workspace_db;
 USE workspace_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
+    id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id),
-    REFERENCES department(id);
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     role_id INT,
-    FOREIGN KEY (role_id),
-    REFERENCES role(id),
     manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,38 +1,38 @@
+USE workspace_db;
+
 INSERT INTO department (name)
 VALUES ('Front-End Development'),
 ('Back-End Development'),
 ('Consumer Relations'),
 ('HR');
 
-INSERT INTO role (title, salary, department_id)
-VALUES ('Lead Front-End Dev', 155000, 1),
-('Front-End Dev', 135000, 1),
-('Lead Back-End Dev', 150000, 2),
-('Node Dev', 100000, 2),
-('Python Dev', 130000, 2),
-('Relations Manager', 100000, 3),
-('Sales', 80000, 3),
-('Social Media Manager', 60000, 3),
-('Customer Support', 70000, 3),
-('HR Manager', 100000, 4),
-('Payroll', 55000, 4),
-('Recruiting', 65000, 4),
-('Internal Affairs', 60000, 4),
+INSERT INTO role (id, title, salary, department_id)
+VALUES (101, 'Lead Front-End Dev', 155000, 1),
+(102, 'Front-End Dev', 135000, 1),
+(201, 'Lead Back-End Dev', 150000, 2),
+(202, 'Node Dev', 100000, 2),
+(203, 'Python Dev', 130000, 2),
+(301, 'Relations Manager', 100000, 3),
+(302, 'Sales', 80000, 3),
+(303, 'Social Media Manager', 60000, 3),
+(304, 'Customer Support', 70000, 3),
+(401, 'HR Manager', 100000, 4),
+(402, 'Payroll', 55000, 4),
+(403, 'Recruiting', 65000, 4),
+(404, 'Internal Affairs', 60000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES
-('James', 'Dunn', 1),
-('Elizabeth', 'Jules', 3),
-('Lisa', 'Yuen', 6),
-('Emily', 'Seer', 10),
-('Carl', 'Lee', 2, 1),
-('Mary', 'Smith', 2, 1),
-('Susan', 'Potters', 2, 1),
-('Dave', 'Lear', 4, 2),
-('Mike', 'Baker', 5, 2),
-('Abdul', 'Romano', 7, 3),
-('Jenny', 'Ross', 8, 3),
-('John', 'Moore', 9, 3),
-('Mark', 'Adams', 11, 4),
-('Blake', 'Anderson', 12, 4),
-('Robert', 'Peters', 13, 4),
+VALUES ('James', 'Dunn', 101, NULL),
+('Elizabeth', 'Jules', 201, NULL),
+('Lisa', 'Yuen', 301, NULL),
+('Emily', 'Seer', 401, NULL),
+('Carl', 'Lee', 102, 1),
+('Mary', 'Smith', 102, 1),
+('Dave', 'Lear', 202, 2),
+('Mike', 'Baker', 203, 2),
+('Abdul', 'Romano', 302, 3),
+('Jenny', 'Ross', 303, 3),
+('John', 'Moore', 304, 3),
+('Mark', 'Adams', 402, 4),
+('Blake', 'Anderson', 403, 4),
+('Robert', 'Peters', 404, 4);
